bb8f04573b6634c9949ba9d25884090fbe9aa28a
Bouberraga <Bouberraga@172.28.179.137>
2024-11-13 08:23:36 +0100

    classe Evaluation

--- CMM/src/location/Evaluation.java
+++ CMM/src/location/Evaluation.java
@@ -3,5 +3,41 @@ package location;
 // A COMPLETER
 
 public class Evaluation {
+	int note;
+	String commentaire;
+	Utilisateur utilis;
+	public Evaluation(int note, String commentaire, Utilisateur utilis) {
+        this.note = note;
+        this.commentaire = commentaire;
+        this.utilis = utilis;
+    }
+	
+    public int getNote() {
+        return note;
+    }
 
+    
+    public void setNote(int note) {
+        this.note = note;
+    }
+
+    
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    
+    public String getUtilisateurPseudo() {
+        return utilis.pseudo();  
+    }
+
+    public void afficherEvaluation() {
+        System.out.println("Note : " + note + "Commentaire : "+ commentaire + "Utilisateur : "+ utilis.pseudo);
+
+    }
 }
