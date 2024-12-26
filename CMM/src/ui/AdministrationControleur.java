e5c88a26447c6f68d69d9373cca0ff5b0b3c7f8e
mokhtar272 <mokhtarrida64@gmail.com>
2024-12-26 12:52:34 +0100

    ajoute teste Gestionnaire

---
+++ CMM/src/ui/AdministrationControleur.java
@@ -0,0 +1,194 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * Controleur JavaFX de la fenêtre d'administration.
+ *
+ * @author Eric Cariou
+ *
+ */
+public class AdministrationControleur {
+  
+  @FXML
+  private CheckBox checkBoxLocationFilm;
+  
+  @FXML
+  private TextField entreeAffiche;
+  
+  @FXML
+  private TextField entreeAnneeFilm;
+  
+  @FXML
+  private TextField entreeNationaliteArtiste;
+  
+  @FXML
+  private TextField entreeNomArtiste;
+  
+  @FXML
+  private TextField entreeNomPrenomRealisateur;
+  
+  @FXML
+  private TextField entreePrenomArtiste;
+  
+  @FXML
+  private TextField entreeTitreFilm;
+  
+  @FXML
+  private Label labelListeArtistes;
+  
+  @FXML
+  private Label labelListeFilms;
+  
+  @FXML
+  private ListView<String> listeArtistes;
+  
+  @FXML
+  private ChoiceBox<String> listeChoixAgeLimite;
+  
+  @FXML
+  private ListView<String> listeFilms;
+  
+  @FXML
+  private ListView<String> listeGenresFilm;
+  
+  @FXML
+  private ListView<String> listeTousGenres;
+  
+  @FXML
+  void actionBoutonAfficherArtistesActeurs(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherArtistesRealisateurs(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherFilmsActeurSelectionne(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherFilmsDuRealisateur(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherFilmsRealisateurSelectionne(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherTousActeursFilm(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherTousArtistes(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAjouterActeurFilm(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAjouterGenreFilm(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonChercherArtiste(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonChercherFilm(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonChoisirArtisteSelectionneRealisateur(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonEnregistrerArtiste(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonEnregistrerFilm(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonNouveauArtiste(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonNouveauFilm(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonParcourirAffiche(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonSupprimerArtiste(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonSupprimerFilm(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionMenuApropos(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionMenuCharger(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionMenuQuitter(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionMenuSauvegarder(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionListeSelectionArtiste(MouseEvent event) {
+    
+  }
+  
+  @FXML
+  void actionListeSelectionFilm(MouseEvent event) {
+    
+  }
+  
+  @FXML
+  void initialize() {
+    // Mettre ici le code d'initialisation du contenu de la fenêtre
+  }
+}
