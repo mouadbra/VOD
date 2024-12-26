05f8ff820c720532a100fbc1d02edf53020d8556
Mouad <mouadbrahmi02@gmail.com>
2024-12-26 13:01:15 +0100

    Sauvegarde des modifications locales

---
+++ CMM/src/ui/MainInterface.java
@@ -0,0 +1,78 @@
+package ui;
+
+import java.io.IOException;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * Classe qui lance l'interface graphique de l'application.
+ *
+ * @author Eric Cariou
+ */
+public final class MainInterface extends Application {
+  
+  /**
+   * Affiche la fenêtre de l'utilisateur pour louer des films.
+   */
+  public void startFenetreEtudiants() {
+    try {
+      URL url = getClass().getResource("utilisateur.fxml");
+      FXMLLoader fxmlLoader = new FXMLLoader(url);
+      VBox root = (VBox) fxmlLoader.load();
+      
+      Scene scene = new Scene(root, 1200, 650);
+      
+      Stage stage = new Stage();
+      stage.setResizable(true);
+      stage.setTitle("Location de films");
+      
+      stage.setScene(scene);
+      stage.show();
+      
+    } catch (IOException e) {
+      System.err.println("Erreur au chargement de la fenêtre utilisateur : " + e);
+    }
+  }
+  
+  /**
+   * Affiche la fenêtre d'administration des films.
+   *
+   * @param primaryStage le paramètre passé par JavaFX pour la fenêtre
+   *        principale
+   */
+  public void startFenetreFormation(Stage primaryStage) {
+    try {
+      URL url = getClass().getResource("administration.fxml");
+      FXMLLoader fxmlLoader = new FXMLLoader(url);
+      VBox root = (VBox) fxmlLoader.load();
+      
+      Scene scene = new Scene(root, 615, 725);
+      
+      primaryStage.setScene(scene);
+      primaryStage.setResizable(true);
+      primaryStage.setTitle("Administration des films");
+      primaryStage.show();
+      
+    } catch (IOException e) {
+      System.err.println("Erreur au chargement de la fenêtre administration : " + e);
+    }
+  }
+  
+  @Override
+  public void start(Stage primaryStage) {
+    
+    // Lancement des 2 fenêtres de l'application
+    this.startFenetreFormation(primaryStage);
+    this.startFenetreEtudiants();
+    
+    // Rajouter ici du code si besoin ou avant le lancement des fenêtres
+  }
+  
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
---
+++ CMM/src/ui/UtilisateurControleur.java
@@ -0,0 +1,228 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+
+/**
+ * Controleur JavaFX de la fenêtre utilisateur.
+ *
+ * @author Eric Cariou
+ *
+ */
+public class UtilisateurControleur {
+  
+  @FXML
+  private CheckBox checkFilmLouable;
+  
+  @FXML
+  private TextField entreeAdresseUtilisateur;
+  
+  @FXML
+  private TextField entreeAgeLimiteFilm;
+  
+  @FXML
+  private TextField entreeAgeUtilisateur;
+  
+  @FXML
+  private TextField entreeAnneeFilm;
+  
+  @FXML
+  private TextField entreeAuteurEvaluation;
+  
+  @FXML
+  private TextField entreeEvaluationMoyenne;
+  
+  @FXML
+  private TextField entreeGenresFilm;
+  
+  @FXML
+  private TextField entreeMotDePasseUtilisateur;
+  
+  @FXML
+  private TextField entreeNationaliteArtiste;
+  
+  @FXML
+  private TextField entreeNomArtiste;
+  
+  @FXML
+  private TextField entreeNomPrenomRealisateurFilm;
+  
+  @FXML
+  private TextField entreeNomUtilisateur;
+  
+  @FXML
+  private TextField entreePrenomArtiste;
+  
+  @FXML
+  private TextField entreePrenomUtilisateur;
+  
+  @FXML
+  private TextField entreePseudoUtilisateur;
+  
+  @FXML
+  private TextField entreeTitreFilm;
+  
+  @FXML
+  private Label labelListeFilms;
+  
+  @FXML
+  private Label labelListeArtistes;
+  
+  @FXML
+  private ListView<String> listeArtistes;
+  
+  @FXML
+  private ListView<String> listeEvaluations;
+  
+  @FXML
+  private ListView<String> listeFilms;
+  
+  @FXML
+  private ListView<String> listeFilmsEnLocation;
+  
+  @FXML
+  private ListView<String> listeGenresFilm;
+  
+  @FXML
+  private ChoiceBox<Integer> listeNoteEvaluation;
+  
+  @FXML
+  private TextArea texteCommentaire;
+  
+  @FXML
+  private StackPane paneAffiche;
+  
+  @FXML
+  void actionBoutonAfficherActeursFilmSelectionne(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherArtistesActeurs(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherArtistesRealisateurs(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherFilmLoue(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherFilmRealisateurSelectionne(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherFilmsActeurSelectionne(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherFilmsGenre(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherFilmsRealisateurSelectionne(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherMonEvaluation(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherTousArtistes(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherTousFilms(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonChercherActeur(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonChercherFilm(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonChercherRealisateur(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonConnexion(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonCreerMonEvaluation(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonDeconnexion(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonFinLocation(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonInscription(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonLouerFilmSelectionne(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonModifierMonEvaluation(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionSelectionArtiste(MouseEvent event) {
+    
+  }
+  
+  @FXML
+  void actionSelectionEvaluation(MouseEvent event) {
+    
+  }
+  
+  @FXML
+  void actionSelectionFilm(MouseEvent event) {
+    
+  }
+  
+  @FXML
+  void initialize() {
+    // Mettre ici le code d'initialisation du contenu de la fenêtre
+  }
+  
+}
